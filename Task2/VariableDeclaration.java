eb5d687373a08e9d8b790c9a21a1edfdb860e341
Ron <wondelrosario@gmail.com>
2022-09-17 05:00:14 +1400

    Add files via upload

---
+++ Task2/VariableDeclaration.java
@@ -0,0 +1,17 @@
+package Task2;
+
+public class VariableDeclaration {
+    public static void main(String[] args) {
+        String name;
+        short noOfParticipants;
+        byte age;
+        String course;
+        byte yearLevel;
+        char section;
+        float tuitionFee;
+        boolean runningPosition;
+        String dMonth;
+        byte dDay;
+        short dYear;
+    }
+}
